ce0ed7d1fee4e35350ea71fccbfb35924ae2e790
toddxu <Todd@w87-175-020.wireless.uvic.ca>
2015-11-17 17:27:55 -0800

    Comments for player.cpp & player.h

--- Player.cpp
+++ Player.cpp
@@ -1,22 +1,41 @@
+/**
+ *Player.cpp inherits GameEntity implementing the header file Player.h
+ */
+
 #include "Player.h"
 
+/**
+ * A default constructor that creates a player by initial id, a name
+ */
 Player::Player(int id, std::string name) : GameEntity(id, name){
     std::cout << "Created a player..." << std::endl;
 }
 
+/**
+ * A standard constructor that creates a player by initial id, a name, and a connection id
+ */
 Player::Player(int id, std::string name, int connectionID) : GameEntity( id, name ) {
     connectionID_ = connectionID;
     std::cout << "Created a player with connection ID..." << std::endl;
 }
 
+/**
+ * A destructor that removes object player
+ */
 Player::~Player() {
     std::cout << "Removed a player..." << std::endl;
 }
 
+/**
+ * A connection id getter that returns the connection id of the player
+ */
 int Player::GetConnectionID() {
     return connectionID_;
 }
 
+/**
+ * Print the information of the player
+ */
 void Player::PrintPlayer(){
 	Print();
     std::cout << connectionID_ << std::endl;
