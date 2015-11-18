a9c8881fb74d38e2bb70fee2cf001ffa7c0f0187
toddxu <Todd@w87-175-020.wireless.uvic.ca>
2015-11-17 17:29:49 -0800

    Comments for GameEntity

--- GameEntity.cpp
+++ GameEntity.cpp
@@ -1,7 +1,9 @@
+/**GameEntity.cpp implements the header file GameEntity.h*/
 #include "GameEntity.h"
 
 #include <iostream>
 
+/**Default constructor that creates a game entity by setting a permanent ID, name, and a blank description. */
 GameEntity::GameEntity(int id, std::string name) {
     id_ = id;
     name_ = name;
@@ -10,6 +12,7 @@ GameEntity::GameEntity(int id, std::string name) {
     std::cout << "Created an entity..." << std::endl;
 }
 
+/**A standard constructor that creates a game entity with a description*/
 GameEntity::GameEntity(int id, std::string name, std::string description) {
     id_ = id;
     name_ = name;
@@ -18,26 +21,32 @@ GameEntity::GameEntity(int id, std::string name, std::string description) {
     std::cout << "Created an entity..." << std::endl;
 }
 
+/**A destructor that removes an entity*/
 GameEntity::~GameEntity() {
     std::cout << "Destroyed an entity..." << std::endl;
 }
 
+/**Get the Id of an entity*/
 int GameEntity::GetId() {
     return id_;
 }
 
+/**Get the name of an entity*/
 std::string GameEntity::GetName() {
 	return name_;
 }
 
+/**Get the description of an entity*/
 std::string GameEntity::GetDescription() {
     return description_;
 }
 
+/**Change or set the description of an entity*/
 void GameEntity::SetDescription(std::string description) {
     description_ = description;
 }
 
+/**Print the ID, name, and description of an entity*/
 void GameEntity::Print() {
     std::cout << id_ << " " << name_ << " " << description_ << std::endl;
 }
